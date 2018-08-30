ba8cfe0272952441f485b0e0a92d164a1558b807
Mark Final <markfinal@hotmail.com>
2018-08-30 22:10:55 +0100

    Bumped to C++11

--- tests/olylumogui/bam/Scripts/olylumogui.cs
+++ tests/olylumogui/bam/Scripts/olylumogui.cs
@@ -10,6 +10,13 @@ namespace olylumogui
 
             var source = this.CreateCxxSourceContainer("$(packagedir)/source/*.cpp");
 
+            source.PrivatePatch(settings =>
+            {
+                var cxxCompiler = settings as C.ICxxOnlyCompilerSettings;
+                cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.Cxx11;
+                cxxCompiler.StandardLibrary = C.Cxx.EStandardLibrary.libcxx;
+            });
+
             if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.OSX))
             {
                 this.CompileAndLinkAgainst<Qt.CoreFramework>(source);
