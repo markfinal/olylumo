b2343f720c191cf042ac6413236f14d6e5463aa4
Mark Final <markfinal@hotmail.com>
2018-08-30 22:01:27 +0100

    Basic module

--- tests/olylumogui/bam/Scripts/olylumogui.cs
+++ tests/olylumogui/bam/Scripts/olylumogui.cs
@@ -1,5 +1,15 @@
 using Bam.Core;
 namespace olylumogui
 {
-    // write modules here ...
+    sealed class OlyLumoGUI :
+        C.Cxx.GUIApplication
+    {
+        protected override void Init(Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCxxSourceContainer("$(packagedir)/source/*.cpp");
+            this.CompileAndLinkAgainst<Qt.Core>(source);
+        }
+    }
 }
---
+++ tests/olylumogui/source/main.cpp
@@ -0,0 +1,6 @@
+#include "QtCore/QtCore"
+
+int main()
+{
+    return 0;
+}
