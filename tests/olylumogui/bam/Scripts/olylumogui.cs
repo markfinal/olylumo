aa6905cef160f7330227bc9295513606afbf7b45
Mark Final <markfinal@hotmail.com>
2018-08-30 22:08:14 +0100

    QtCore is a framework on macOS

--- tests/olylumogui/bam/Scripts/olylumogui.cs
+++ tests/olylumogui/bam/Scripts/olylumogui.cs
@@ -1,7 +1,7 @@
 using Bam.Core;
 namespace olylumogui
 {
-    sealed class OlyLumoGUI :
+    sealed class olylumoGUI :
         C.Cxx.GUIApplication
     {
         protected override void Init(Module parent)
@@ -9,7 +9,15 @@ namespace olylumogui
             base.Init(parent);
 
             var source = this.CreateCxxSourceContainer("$(packagedir)/source/*.cpp");
-            this.CompileAndLinkAgainst<Qt.Core>(source);
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.OSX))
+            {
+                this.CompileAndLinkAgainst<Qt.CoreFramework>(source);
+            }
+            else
+            {
+                this.CompileAndLinkAgainst<Qt.Core>(source);
+            }
         }
     }
 }
