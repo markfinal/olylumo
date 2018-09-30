febb66eed85e81b7ce2d130aa5040de1233c4d53
Mark Final <markfinal@hotmail.com>
2018-09-30 18:31:14 +0100

    Stubbing vector 4 support

---
+++ packages/olylumoray/include/olylumoray/vec4.h
@@ -0,0 +1,21 @@
+#ifndef OLYLUMORAY_VEC4_H
+#define OLYLUMORAY_VEC4_H
+
+namespace olylumoray
+{
+
+class Vec4
+{
+public:
+    Vec4();
+
+private:
+    float _x;
+    float _y;
+    float _z;
+    float _w;
+};
+
+} // namespace olylumoray
+
+#endif // OLYLUMORAY_VEC4_H
---
+++ packages/olylumoray/source/vec4.cpp
@@ -0,0 +1,14 @@
+#include "olylumoray/vec4.h"
+
+namespace olylumoray
+{
+
+Vec4::Vec4()
+    :
+    _x(0),
+    _y(0),
+    _z(0),
+    _w(1)
+{}
+
+} // namespace olylumoray
