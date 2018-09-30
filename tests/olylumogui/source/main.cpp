2548b0f4bc2804bb22d61a808c549c543f147883
Mark Final <markfinal@hotmail.com>
2018-09-30 15:08:02 +0100

    Add function to invoke the ray cast

--- tests/olylumogui/source/main.cpp
+++ tests/olylumogui/source/main.cpp
@@ -2,13 +2,26 @@
 #include "QtWidgets/QMainWindow"
 #include "QtWidgets/QToolBar"
 
-int main(int argc, char *argv[])
+namespace
+{
+
+void
+do_ray_cast()
+{
+}
+
+} // anonymous namespace
+
+int
+main(
+    int argc,
+    char *argv[])
 {
     QApplication app(argc, argv);
     QMainWindow window;
     auto toolbar = window.addToolBar("Rendering");
     auto rayTrace = toolbar->addAction("Ray trace");
-    (void)rayTrace;
+    QObject::connect(rayTrace, &QAction::triggered, do_ray_cast);
     auto pathTrace = toolbar->addAction("Path trace");
     pathTrace->setEnabled(false);
     window.show();
