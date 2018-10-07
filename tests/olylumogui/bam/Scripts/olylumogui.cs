1fa963f5177151f1f4728df8b45764f6c2388916
Mark Final <markfinal@hotmail.com>
2018-10-07 15:05:14 +0100

    Enable exceptions

--- tests/olylumogui/bam/Scripts/olylumogui.cs
+++ tests/olylumogui/bam/Scripts/olylumogui.cs
@@ -30,6 +30,7 @@ namespace olylumogui
             {
                 if (settings is C.ICxxOnlyCompilerSettings cxxCompiler)
                 {
+                    cxxCompiler.ExceptionHandler = C.Cxx.EExceptionHandler.Asynchronous;
                     cxxCompiler.EnableRunTimeTypeInfo = false;
                     cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.Cxx11;
                     cxxCompiler.StandardLibrary = C.Cxx.EStandardLibrary.libcxx;
